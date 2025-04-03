736eff2ad8d4f1f710d0bb5be954a033bbddb9bf
Allison <allisonpb07@gmail.com>
2025-04-03 13:23:52 -0600

    Cambio en los filtros

--- src/main/java/com/tiendaOctavius/controller/PruebasController.java
+++ src/main/java/com/tiendaOctavius/controller/PruebasController.java
@@ -46,9 +46,6 @@ public class PruebasController {
         return "/pruebas/listado2";
     }
 
-    
-
-
 
     @PostMapping("/query1")
     public String query1(
@@ -56,32 +53,6 @@ public class PruebasController {
             @RequestParam("precioSup") double precioSup,
             Model model) {
         var lista = productoService.consultaAmpliada(precioInf, precioSup);
-        model.addAttribute("productos", lista);
-        model.addAttribute("precioInf", "precioInf");
-        model.addAttribute("precioSup", "precioSup");
-
-        return "/pruebas/listado2";
-    }
-
-    @PostMapping("/query2")
-    public String query2(
-            @RequestParam("precioInf") double precioInf,
-            @RequestParam("precioSup") double precioSup,
-            Model model) {
-        var lista = productoService.consultaJPQL(precioInf, precioSup);
-        model.addAttribute("productos", lista);
-        model.addAttribute("precioInf", "precioInf");
-        model.addAttribute("precioSup", "precioSup");
-
-        return "/pruebas/listado2";
-    }
-
-    @PostMapping("/query3")
-    public String query3(
-            @RequestParam("precioInf") double precioInf,
-            @RequestParam("precioSup") double precioSup,
-            Model model) {
-        var lista = productoService.consultaSQL(precioInf, precioSup);
         model.addAttribute("productos", lista);
         model.addAttribute("precioInf", "precioInf");
         model.addAttribute("precioSup", "precioSup");
